527a6c1f26b28a0dc3d1479e8bde30d21dfe9425
Davidodari <davidkibzodari@gmail.com>
2019-08-04 15:33:45 +0300

    add navigation

--- app/src/main/java/com/blaccoder/travelmantics/ui/MainActivity.kt
+++ app/src/main/java/com/blaccoder/travelmantics/ui/MainActivity.kt
@@ -5,8 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.blaccoder.travelmantics.R
-import com.blaccoder.travelmantics.RC_SIGN_IN
-import com.blaccoder.travelmantics.providers
+import com.blaccoder.travelmantics.utils.RC_SIGN_IN
+import com.blaccoder.travelmantics.utils.providers
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
