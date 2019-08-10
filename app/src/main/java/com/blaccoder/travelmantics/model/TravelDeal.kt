869f9d7429479b0854f1c7dd5e8289fdffa4ba62
Davidodari <davidkibzodari@gmail.com>
2019-08-10 11:39:44 +0300

    add firestore add and delete func

--- app/src/main/java/com/blaccoder/travelmantics/model/TravelDeal.kt
+++ app/src/main/java/com/blaccoder/travelmantics/model/TravelDeal.kt
@@ -1,5 +1,8 @@
 package com.blaccoder.travelmantics.model
 
+import java.sql.Timestamp
+import java.util.*
+
 /**
  * Created By David Odari
  * On 01/08/19
@@ -11,4 +14,23 @@ data class TravelDeal(
     val price: String,
     val imageUrl: String,
     val id: Long = 0L
+)
+
+fun TravelDeal.withTimeStamp(): TravelDealTimestamped {
+    return TravelDealTimestamped(
+        title = this.title,
+        description = this.description,
+        price = this.price,
+        imageUrl = this.imageUrl,
+        id = this.id
+    )
+}
+
+data class TravelDealTimestamped(
+    val title: String,
+    val description: String,
+    val price: String,
+    val imageUrl: String,
+    val id: Long = 0L,
+    val timeStamp: String = Timestamp(Date().time).toString()
 )
